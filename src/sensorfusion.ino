410057887c67731faa84a504adaacfe6462a018f
Hyunseup Jo <hyunseup0815@gmail.com>
2023-02-13 16:56:28 +0900

    2023-02-13

--- src/sensorfusion.ino
+++ src/sensorfusion.ino
@@ -1,6 +1,10 @@
 #include "MPU9250.h"
 #include "string.h"
+#include "math.h"
 
+#define PI 3.141592
+
+// raw data
 typedef struct {
   float accel[3]; // raw accel value
   float gyro[3]; // raw gyro value
@@ -15,17 +19,28 @@ typedef struct {
   float filtered_mag[3]; // filtered mag value
 }imu_data;
 
-// an MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
-MPU9250 IMU(Wire, 0x68);
-imu_data mpu9250;
+// yaw calculation parameters(Gyroscope)
+float prevPhi = 0.0;
+float prevTheta = 0.0;
+float prevPsi = 0.0;
+float phi = 0.0;
+float theta = 0.0;
+float psi = 0.0;
+float gyro_yaw = 0.0;
+float dt = (1.0 / 50.0); // 50Hz
 
-int status;
-char* raw_string[3] = {"raw Accel/Gyro/Mag X", "raw Accel/Gyro/Mag Y", "raw Accel/Gyro/Mag Z"};
-char* filtered_string[3] = {"filtered Accel/Gyro/Mag X", "filtered Accel/Gyro/Mag Y", "filtered Accel/Gyro/Mag Z"};
+// yaw calculation parameters(Magnetometer)
+double mag_yaw = 0.0;
 
+// lowpass filter parameter
 static float alpha = 0.8;
 
-void get_raw_data(imu_data *imu)
+// init
+MPU9250 IMU(Wire, 0x68);
+int status;
+imu_data mpu9250;
+
+void get_raw_data(imu_data *imu) // rad/sec
 {
   imu->accel[0] = IMU.getAccelX_mss();
   imu->accel[1] = IMU.getAccelY_mss();
@@ -40,12 +55,12 @@ void get_raw_data(imu_data *imu)
   imu->mag[2] = IMU.getMagZ_uT();
 }
 
-void lowpass_filter(imu_data *imu)
+void lpf(imu_data *imu) // rad/sec
 {
   for(int i = 0; i < 3; i++)
   {
     imu->filtered_accel[i] = (alpha * imu->prev_accel[i]) + ((1.0 - alpha) * imu->accel[i]); 
-    imu->filtered_gyro[i] = (alpha * imu->prev_gyro[i]) + ((1.0 - alpha) * imu->gyro[i]); 
+    imu->filtered_gyro[i] = ((alpha * imu->prev_gyro[i]) + ((1.0 - alpha) * imu->gyro[i]));
     imu->filtered_mag[i] = (alpha * imu->prev_mag[i]) + ((1.0 - alpha) * imu->mag[i]); 
   }
 
@@ -57,29 +72,37 @@ void lowpass_filter(imu_data *imu)
   }
 }
 
+void euler_gyro(imu_data *imu)
+{
+  double sinPhi = sin(prevPhi);
+  double cosPhi = cos(prevPhi);
+  double cosTheta = cos(prevTheta);
+  double tanTheta = tan(prevTheta);
+
+  // Gyro to Euler Angle
+  phi = prevPhi + dt*(imu->filtered_gyro[0] + imu->filtered_gyro[1]*sinPhi*tanTheta + imu->filtered_gyro[2]*cosPhi*tanTheta);
+  theta = prevTheta + dt*(imu->filtered_gyro[1]*cosPhi - imu->filtered_gyro[2]*sinPhi);
+  psi = prevPsi + dt*(imu->filtered_gyro[1]*sinPhi/cosTheta + imu->filtered_gyro[2]*cosPhi/cosTheta);
+  
+  prevPhi = phi;
+  prevTheta = theta;
+  prevPsi = psi;
+
+  gyro_yaw = psi * (180.0 / PI); // radian to degree
+}
+
+void euler_mag(imu_data *imu)
+{
+  mag_yaw = atan2(imu->filtered_mag[1], imu->filtered_mag[0]) * (180.0 / PI); // radian to degree
+}
+
 void serial_print(imu_data *imu)
 {
-  for(int i = 0; i < 3; i++)
-  {
-    Serial.println(raw_string[i]);
+  Serial.print("Gyro yaw: ");
+  Serial.print(gyro_yaw);
 
-    Serial.print(imu->accel[i], 6);
-    Serial.print("\t");
-    Serial.print(imu->gyro[i], 6);
-    Serial.print("\t");
-    Serial.print(imu->mag[i], 6);
-    Serial.print("\t");
-    Serial.println("\n");
-
-    Serial.println(filtered_string[i]);
-    Serial.print(imu->filtered_accel[i], 6);
-    Serial.print("\t");
-    Serial.print(imu->filtered_gyro[i], 6);
-    Serial.print("\t");
-    Serial.print(imu->filtered_mag[i], 6);
-    Serial.print("\t");
-    Serial.println("\n");
-  }
+  Serial.print("Mag yaw: ");
+  Serial.println(mag_yaw);
 }
 
 void set_memory(imu_data *imu)
@@ -115,13 +138,18 @@ void setup() {
     IMU.setAccelRange(MPU9250::ACCEL_RANGE_8G);
 
     // setting the gyroscope full scale range to +/-500 deg/s
-    IMU.setGyroRange(MPU9250::GYRO_RANGE_500DPS);
+    IMU.setGyroRange(MPU9250::GYRO_RANGE_250DPS);
 
     // setting DLPF bandwidth to 20 Hz
     IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_20HZ);
 
     // setting SRD to 19 for a 50 Hz update rate
     IMU.setSrd(19);
+
+    /* finds bias and scale factor calibration for the magnetometer,
+    The sensor should be rotated in a figure 8 motion until complete */
+    IMU.calibrateGyro();
+    IMU.calibrateMag();
     
     // set struct variables to 0
     set_memory(&mpu9250);
@@ -131,6 +159,10 @@ void setup() {
 void loop() {
   IMU.readSensor(); // read raw data
   get_raw_data(&mpu9250); // allocate raw data to struct
-  lowpass_filter(&mpu9250); // filtered data
-  serial_print(&mpu9250); // print filtered data
+  lpf(&mpu9250); // filtered data
+
+  euler_gyro(&mpu9250);
+  euler_mag(&mpu9250);
+
+  serial_print(&mpu9250); // print yaw_data
 }
